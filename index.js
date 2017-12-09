9a6e6720ac41d714de181450f0c021b321d9ea4e
Obed Orduño <oorduno@NSL-JORDUNOs-MacBook-Pro.local>
2017-12-08 17:48:34 -0700

    2.- where(spec) method

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ Array.prototype.each = function(callback){
     callback.call(this, this[i], i);
   }
 }
+
+Array.prototype.where = function(callback){
+  var result = [];
+
+  for(var i = 0; i < this.length; i++){
+    if(callback.call(this, this[i])){
+      result.push(this[i]);
+    } 
+  }
+  return result;
+}
